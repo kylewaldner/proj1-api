41054273e76c16bac57f04c738ebdf0ba7daa1cd
Kyle Waldner <kyle.t.waldner@gmail.com>
2022-01-27 21:28:44 -0500

    init

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+# route
+@app.route('/')
+# route function
+def home():
+  # send 'hey!'
+  return 'nothing is here'
+
+
+@app.route('/fire')
+def fire():
+  return 'yes'
+
+# listen
+if __name__ == "__main__":
+  if os.environ['FLASK_ENV'] == 'development':
+    print("dev mode")
+    app.run(host='0.0.0.0', port=os.environ['PORT_A']) # 56811
+  else:
+    app.run(host='0.0.0.0')
+  # app.run(port=3000, debug=True)
